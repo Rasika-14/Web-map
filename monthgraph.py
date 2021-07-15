5060936fa9d227639015d32309ce8170ebf86c73
Rasika-14 <70766264+Rasika-14@users.noreply.github.com>
2021-07-15 15:41:51 +0530

    Add files via upload

---
+++ monthgraph.py
@@ -0,0 +1,82 @@
+import justpy as jp
+import pandas
+from datetime import datetime
+from pytz import utc
+ 
+data = pandas.read_csv("reviews.csv", parse_dates=['Timestamp'])
+data['Month'] = data['Timestamp'].dt.strftime('%Y-%m')
+month_average = data.groupby(['Month']).mean()
+ 
+chart_def = """
+{
+    chart: {
+        type: 'spline',
+        inverted: false
+    },
+    title: {
+        text: 'Average Rating by Month'
+    },
+    subtitle: {
+        text: 'According to the Course Reviews Dataset'
+    },
+    xAxis: {
+        reversed: false,
+        title: {
+            enabled: true,
+            text: 'Month'
+        },
+        labels: {
+            format: '{value} km'
+        },
+        accessibility: {
+            rangeDescription: 'Range: 0 to 80 km.'
+        },
+        maxPadding: 0.05,
+        showLastLabel: true
+    },
+    yAxis: {
+        title: {
+            text: 'Rating'
+        },
+        labels: {
+            format: '{value}°'
+        },
+        accessibility: {
+            rangeDescription: 'Range: -90°C to 20°C.'
+        },
+        lineWidth: 2
+    },
+    legend: {
+        enabled: false
+    },
+    tooltip: {
+        headerFormat: '<b>{series.name}</b><br/>',
+        pointFormat: '{point.x} km: {point.y}°C'
+    },
+    plotOptions: {
+        spline: {
+            marker: {
+                enable: false
+            }
+        }
+    },
+    series: [{
+        name: 'Rating',
+        data: [[0, 15], [10, -50], [20, -56.5], [30, -46.5], [40, -22.1],
+            [50, -2.5], [60, -27.7], [70, -55.7], [80, -76.5]]
+    }]
+}
+"""
+ 
+def app1():
+    wp1 = jp.QuasarPage()
+    h1 = jp.QDiv(a=wp1, text="Analysis of Course Reviews", classes="text-h3 text-center q-pa-md")
+    p1 = jp.QDiv(a=wp1, text="These graphs represent course review analysis")
+    
+    hc1 = jp.HighCharts(a=wp1, options=chart_def)
+    hc1.options.xAxis.categories = list(month_average.index)
+    hc1.options.series[0].data = list(month_average['Rating'])
+ 
+    return wp1
+ 
+jp.justpy(app1)
